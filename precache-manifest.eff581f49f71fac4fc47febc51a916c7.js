97284346e9ac3392376ec4847014aed65d47e2bc
abdulrehman ajmal <abdulrehman.ajmal@outlook.com>
2020-01-30 01:47:25 +0500

    Updates

---
+++ precache-manifest.eff581f49f71fac4fc47febc51a916c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "444d33fbbcc30df7d75c6f94810826cb",
+    "url": "/TODO-APP/index.html"
+  },
+  {
+    "revision": "4afb3d5cd8a882b5de08",
+    "url": "/TODO-APP/static/css/main.639529c9.chunk.css"
+  },
+  {
+    "revision": "95b3c303b92f2f50b9e1",
+    "url": "/TODO-APP/static/js/2.28b0af81.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/TODO-APP/static/js/2.28b0af81.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4afb3d5cd8a882b5de08",
+    "url": "/TODO-APP/static/js/main.54568cb8.chunk.js"
+  },
+  {
+    "revision": "091cd3ea509445336690",
+    "url": "/TODO-APP/static/js/runtime-main.c82f0dff.js"
+  },
+  {
+    "revision": "7381c9522b188a5e42747c6f2eceb39c",
+    "url": "/TODO-APP/static/media/pattern.7381c952.png"
+  }
+]);
